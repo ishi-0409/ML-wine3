d5c5650241712a8c52038adda8b20280246653ce
ishi-0409 <isaku0409@icloud.com>
2025-09-26 18:07:00 +0900

    code

---
+++ wine2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold,cross_val_score
+
+df = pd.read_csv('winequality-red.csv',sep=';')
+
+x = df.drop('quality',axis=1)
+y = df['quality']
+
+model = LinearRegression()
+
+kf = KFold(n_splits=15,shuffle=True)
+
+scores = cross_val_score(model,x,y,cv=kf,scoring='r2')
+
+print('各分割における決定係数:',scores)
+print('平均決定係数:',scores.mean())
